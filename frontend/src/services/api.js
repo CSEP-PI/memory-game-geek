4cc6d99ecd4e76983f54042572fc53b423d249d3
Weslley Costa <weslleycosta.ti@gmail.com>
2025-08-22 15:58:47 -0300

    fix:docker backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://31.97.175.41:8001/api';
+//const API_URL = import.meta.env.VITE_API_URL || 'http://31.97.175.41:8001/api';
+const API_URL = 'http://31.97.175.41:8001/api';
+
 
 const api = axios.create({
     baseURL: API_URL,
